ec1a8c1defc33bb8d0c62902e0114de56d5a2844
Michael Young <mikeyoung13@gmail.com>
2011-02-26 10:36:45 -0500

    migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110226153232) do
+ActiveRecord::Schema.define(:version => 20110226153546) do
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "line_items", :force => true do |t|
     t.integer  "product_id"
